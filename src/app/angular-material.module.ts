53ee1a699f6b5206a9e197da43c625fd83dbd829
Samuel de Carvalho <samueldcarvalho99@gmail.com>
2023-05-30 01:19:35 -0300

    feat(front): projects section

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import {MatChipsModule} from '@angular/material/chips';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
-const modules = [MatButtonModule, MatIconModule, MatChipsModule];
+const modules = [
+  MatButtonModule,
+  MatIconModule,
+  MatChipsModule,
+  MatProgressBarModule,
+  MatTooltipModule,
+];
 
 @NgModule({
   imports: modules,
